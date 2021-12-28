172bc8d28afc31d12b0337c01671d5dbdcdcfad1
Doğan Can Selek <canselek2434@gmail.com>
2021-12-28 11:03:34 +0300

    add loading func

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -4,19 +4,27 @@ import { useParams } from 'react-router-dom'
 
 function User() {
     const [userInfo, setUserInfo] = useState([])
+    const [loading, setLoading] = useState(false);
     let { id } = useParams();
 
     useEffect(() => {
         axios(`https://jsonplaceholder.typicode.com/users/${id}`)
             .then(res => setUserInfo(res.data))
+            .finally(setLoading(true))
     }, [id])
     return (
         <div>
-            <code>{userInfo.name}</code>
-            <br/>
-            <code>{userInfo.email}</code>
-            <br/>
-            <code>{userInfo.phone}</code>
+            {!loading && <h1>......</h1>}
+            {loading &&
+                <>
+                    <h3>kullanıcı {userInfo.id}</h3>
+                    <code>{userInfo.name}</code>
+                    <br />
+                    <code>{userInfo.email}</code>
+                    <br />
+                    <code>{userInfo.phone}</code>
+                </>
+            }
         </div>
     )
 }
